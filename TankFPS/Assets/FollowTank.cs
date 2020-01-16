7ceb54592345180fbf901d749b4518888a58974d
George Waller <george.waller3@gmail.com>
2020-01-16 09:23:42 +0000

    New Tank Asset

---
+++ TankFPS/Assets/FollowTank.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    //player position
+    public Transform player;
+
+    // offset behind player
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // move camera to player position
+        transform.position = player.position + offset;
+    }
+}
