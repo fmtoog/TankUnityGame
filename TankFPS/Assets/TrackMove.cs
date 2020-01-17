1d80f37e43cf3811f9a0f7d7bcd4581184f8f15c
George Waller <george.waller3@gmail.com>
2020-01-17 13:45:52 +0000

    Friday

--- TankFPS/Assets/TrackMove.cs
+++ TankFPS/Assets/TrackMove.cs
@@ -4,23 +4,20 @@ using UnityEngine;
 
 public class TrackMove : MonoBehaviour
 {
-    public Transform tank;
-
-    // Start is called before the first frame update
+    private Vector3 lastPos;
+    public float wheelRadius = 1; // you need to set this based on your model
+    private Transform child; // your visual mesh should be the first child of this object
     void Start()
     {
-        
+        lastPos = transform.position;
+        child = transform.GetChild(0);
     }
-
-    // Update is called once per frame
     void Update()
     {
-        //https://answers.unity.com/questions/26856/check-object-for-motion.html
-        curPos = position;
-        if (curPos == lastPos)
-        {
-            print("Not moving");
-        }
-        lastPos = curPos;
+        Vector3 movement = transform.position - lastPos;
+        float distance = Vector3.Dot(movement, transform.forward);
+        float spin = (distance / (2f * Mathf.PI * wheelRadius)) * 360f;
+        child.Rotate(new Vector3(-spin, 0, 0)); // assuming your wheel models are facing z-forward, y-up
+        lastPos = transform.position;
     }
 }
