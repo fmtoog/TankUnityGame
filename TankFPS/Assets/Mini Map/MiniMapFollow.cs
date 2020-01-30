de637bdfd7860bfed6bf5c098ffe757a348205a2
Epic1121 <06jkeeling@brightoncollege.net>
2020-01-30 09:27:26 +0000

    Here is the mini-map
    It works on an older version, i.e. without the bullet

---
+++ TankFPS/Assets/Mini Map/MiniMapFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapFollow : MonoBehaviour
+{
+    public GameObject TankToFollow;
+    public float offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(TankToFollow.transform.position.x, TankToFollow.transform.position.y + offset, TankToFollow.transform.position.z);
+    }
+}
